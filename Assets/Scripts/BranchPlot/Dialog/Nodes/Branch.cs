d87d2067f4bf19ca7ccd8ec5466bcd3f80f8bf5a
Yu <Vmasks@outlook.com>
2023-10-13 15:36:22 +0800

    做了分支对话xNode的示例

---
+++ Assets/Scripts/BranchPlot/Dialog/Nodes/Branch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class Branch : Node {
+	[Input]
+	public Empty input;
+	[Output]
+	public Empty posOutput;
+	[Output]
+	public Empty negOutput;
+	// Use this for initialization
+	public string positive;
+	public string negative;
+	protected override void Init() {
+		base.Init();
+		
+	}
+
+	// Return the correct value of an output port when requested
+	public override object GetValue(NodePort port) {
+		return null; // Replace this
+	}
+}
---
+++ Assets/Scripts/BranchPlot/Dialog/Nodes/Chat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class Chat : Node {
+	[Input]
+	public Empty input;
+	[Output]
+	public Empty output;
+	[TextArea]
+	public List<string> content;
+
+	// Use this for initialization
+	protected override void Init() {
+		base.Init();
+		
+		
+	}
+
+	// Return the correct value of an output port when requested
+	public override object GetValue(NodePort port) {
+		return null; // Replace this
+	}
+}
